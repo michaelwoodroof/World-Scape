93ccb1cd86918b8f14f8e8c813678d6c3677b478
michael woodroof <michaelwoodroof@outlook.com>
2020-11-21 13:55:03 +0000

    Overhaul of Dark Mode

--- app/src/main/java/com/michaelwoodroof/worldscape/ui/AddChipBottomDialogFragment.kt
+++ app/src/main/java/com/michaelwoodroof/worldscape/ui/AddChipBottomDialogFragment.kt
@@ -10,6 +10,9 @@ import com.michaelwoodroof.worldscape.R
 
 class AddChipBottomDialogFragment : BottomSheetDialogFragment() {
 
+    // @TODO Maybe Add Suggestions from Positive / Negative Traits in strings
+    // @TODO Adjust Keyboard to be just above Add Trait Button
+
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         return inflater.inflate(R.layout.bottom_sheet_new_chip, container, false)
     }
